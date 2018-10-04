e462b6c416c590dfa4c451efad5c9c87f620e995
Fernmu <35671996+Fernmu@users.noreply.github.com>
2018-10-04 13:25:07 -0500

    Update SPI.c

--- SPI.c
+++ SPI.c
@@ -6,9 +6,10 @@
  */
 #include "SPI.h"
 #include "GPIO.h"
+#include "Bits.h"
 
 /*Enables the clock*/
-static void SPI_enable_clock(spi_channel_t channel){
+static void SPI_enable_clk(spi_channel_t channel){
 	switch(channel){
 			case SPI_0:
 				SPI0->MCR &= ~(SPI_MCR_MDIS_MASK);
@@ -202,17 +203,17 @@ static void SPI_clock_phase(spi_channel_t channel, spi_phase_t cpha){
 
 static void SPI_baud_rate(spi_channel_t channel, uint32_t baudRate){
 	switch(channel){
-			case SPI_0:
-				SPI0->CTAR[0] |= baudRate;
-				break;
-			case SPI_1:
-				SPI1->CTAR[0] |= baudRate;
-				break;
-			case SPI_2:
-				SPI2->CTAR[0] |= baudRate;
-				break;
-			default:
-				break;
+		case SPI_0:
+			SPI0->CTAR[0] |= baudRate;
+		break;
+		case SPI_1:
+			SPI1->CTAR[0] |= baudRate;
+		break;
+		case SPI_2:
+			SPI2->CTAR[0] |= baudRate;
+		break;
+		default:
+		break;
 	}
 }
 
@@ -223,14 +224,17 @@ static void SPI_msb_first(spi_channel_t channel, spi_lsb_or_msb_t msb){
 					SPI0->CTAR[0] |= SPI_CTAR_LSBFE_MASK;
 				}
 				else{
-					SPI0->CTAR[0] &= ~(SPI_CTAR_LSBFE_MASK);
+
+					  SPI0->CTAR[0] &= ~(SPI_CTAR_LSBFE_MASK);
 				}
 			break;
 			case SPI_1:
 				if(SPI_MSB == msb){
+
 				  SPI1->CTAR[0] |= SPI_CTAR_LSBFE_MASK;
 				}
 				else{
+
 				  SPI1->CTAR[0] &= ~(SPI_CTAR_LSBFE_MASK);
 				}
 			break;
@@ -242,6 +246,7 @@ static void SPI_msb_first(spi_channel_t channel, spi_lsb_or_msb_t msb){
 				  SPI2->CTAR[0] &= ~(SPI_CTAR_LSBFE_MASK);
 				}
 			break;
+
 		}
 }
 
@@ -268,19 +273,7 @@ void SPI_start_tranference(spi_channel_t channel){
 }
 
 void SPI_stop_tranference(spi_channel_t channel){
-	switch(channel){
-		case SPI_0:
-			SPI0->MCR |= SPI_MCR_HALT_MASK;
-			break;
-		case SPI_1:
-			SPI1->MCR |= SPI_MCR_HALT_MASK;;
-			break;
-		case SPI_2:
-			SPI2->MCR |= SPI_MCR_HALT_MASK;
-			break;
-		default:
-			break;
-		}
+
 }
 
 /*A read access of PUSHR returns the topmost TX FIFO entry.*/
@@ -310,16 +303,30 @@ void SPI_send_one_byte(spi_channel_t channel, uint8_t Data){
 		}
 }
 
-void SPI_init(const spi_config_t* SPI_config){
-	SPI_clk(SPI_config->spi_channel);
-	GPIO_clock_gating(SPI_config->spi_gpio_port.gpio_port_name);
-	GPIO_pin_control_register(SPI_config->spi_gpio_port.gpio_port_name, SPI_config->spi_gpio_port.spi_clk, &(SPI_config->spi_gpio_port.gpio_port_name(GPIO_D)));
-	GPIO_pin_control_register(SPI_config->spi_gpio_port.gpio_port_name, SPI_config->spi_gpio_port.spi_sout, &(SPI_config->spi_gpio_port.gpio_port_name(GPIO_D)));
-	SPI_set_master(SPI_config->spi_channel, SPI_config->spi_master);
-	SPI_fifo(SPI_config->spi_channel, SPI_config->spi_enable_fifo);
-	SPI_enable_clock(SPI_config->spi_channel);
-	SPI_clock_polarity(SPI_config->spi_channel, SPI_config->spi_polarity);
-	SPI_frame_size(SPI_config->spi_channel, SPI_config->spi_frame_size);
-	SPI_clock_phase(SPI_config->spi_channel, SPI_config->spi_phase);
-	SPI_baud_rate(SPI_config->spi_channel, SPI_config->spi_baudrate);
+
+
+
+
+void SPI_init(const spi_config_t* spi_config)
+{
+ gpio_pin_control_register_t enable_bit = GPIO_MUX1;
+ SPI_clk(spi_config->spi_channel);
+ GPIO_clock_gating(spi_config->spi_gpio_port.gpio_port_name);
+ GPIO_pin_control_register(spi_config->spi_gpio_port.gpio_port_name, spi_config->spi_gpio_port.spi_clk, &(spi_config->pin_config));
+ GPIO_pin_control_register(spi_config->spi_gpio_port.gpio_port_name ,spi_config->spi_gpio_port.spi_sout ,&(spi_config->pin_config));
+ SPI_set_master(spi_config->spi_channel,spi_config->spi_master);
+ SPI_fifo(spi_config->spi_channel, spi_config->spi_enable_fifo);
+ SPI_enable_clk(spi_config->spi_channel);
+ SPI_clock_polarity(spi_config->spi_channel, spi_config->spi_polarity);
+ SPI_frame_size(spi_config->spi_channel,spi_config->spi_frame_size);
+ SPI_clock_phase(spi_config->spi_channel, spi_config->spi_phase);
+ SPI_baud_rate(spi_config->spi_channel, spi_config->spi_baudrate);
+ SPI_msb_first(spi_config->spi_channel, spi_config->spi_lsb_or_msb);
+
+ GPIO_pin_control_register(GPIO_D, bit_4, &enable_bit);
+ GPIO_set_pin(GPIO_D, bit_4);
+ GPIO_data_direction_pin(GPIO_D, GPIO_OUTPUT, bit_4);
 }
+
+
+
